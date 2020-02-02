96374a9efba8242c30b72334299f81bd6320ee24
Raikuns <joopwestra@gmail.com>
2020-02-02 11:01:03 +0100

    Laser begin +update

---
+++ Project Motherbrain/Assets/Scripts/HammerAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAttack : MonoBehaviour
+{
+    public BossBattle boss;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() == true)
+        {
+            boss.TakeDamage(20);
+        }
+    }
+}
