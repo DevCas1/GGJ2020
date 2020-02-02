67fc075fa06e499b4be9bbbeaf54f4ea28614d10
Sjouke Yntema <suyntema@live.nl>
2020-02-02 10:12:12 +0100

    Reimport Shield

---
+++ Project Motherbrain/Assets/Scripts/Gear/Implementations/ShieldArm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ShieldArm : ArmsEquipment
+{
+    private bool _isActive;
+
+    public override void Attach(PlayerController controller, PlayerGear gear)
+    {
+        PlayerController = controller;
+        PlayerGear = gear;
+        PlayerRigidbody = controller.Rigidbody;
+    }
+
+    private void Activate()
+    {
+        PlayerController.Boost();
+    }
+
+    private void Deactivate()
+    {
+        PlayerController.ExitBoost();
+    }
+
+    public override void Use()
+    {
+        _isActive = !_isActive;
+
+        if (_isActive)
+            Activate();
+        else
+            Deactivate();
+    }
+}
